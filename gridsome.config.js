73ebaa482fb18c8d495c76bdac9792b593991681
Almadih <modialmadih@gmail.com>
2022-12-24 19:55:04 +0200

    new post

--- gridsome.config.js
+++ gridsome.config.js
@@ -17,7 +17,7 @@ module.exports = {
       options: {
         typeName: 'Post',
         path: 'content/posts/*.md',
-        route: '/:slug',
+        route: '/:title',
         refs: {
           // Creates a GraphQL collection from 'tags' in front-matter and adds a reference.
           tags: {
